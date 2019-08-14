ab1f4bbae6550ea8e6feafde4ba03a3cd1401ed9
Rita Mozenaite <rita.mozenaite@gmail.com>
2019-08-14 09:51:05 +0300

    add footer

--- Public/index.php
+++ Public/index.php
@@ -68,5 +68,37 @@ $tasks = mysqli_query($db, "SELECT * FROM tasks");
     </tbody>
 </table>
 
+<div class="footer-main-div">
+                         
+                    <div class="footer-social-icons">
+                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                        <ul>
+                            <li><a href="#" target="blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" target="blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" target="blank"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#" target="blank"><i class="fa fa-youtube"></i></a></li>
+                        </ul>
+                        </div>
+
+                        <div class="footer-menu-one">
+                            <ul>
+                              <li><a href="#">Home</a></li>
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Products</a></li>
+                                <li><a href="#">Contact us</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="footer-menu-two">
+                                <ul>
+                                  <li><a href="#">Blog</a></li>
+                                    <li><a href="#">News</a></li>
+                                    <li><a href="#">Gallery</a></li>
+                                    <li><a href="#">Media</a></li> 
+                                </ul>
+                            </div>
+                           
+                        </div>
 </body>
 </html>
